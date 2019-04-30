47b653a1ef515179389ec74d3790c2ea8720941d
Diego Andrés <dabaquerot@unal.edu.co>
2019-04-29 23:51:51 -0500

    Feat: Add repo readme reference

--- UNBlockchainApp/UNBlockchainApp/UNBlockchainApp/Views/DocumentationPage.xaml.cs
+++ UNBlockchainApp/UNBlockchainApp/UNBlockchainApp/Views/DocumentationPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace UNBlockchainApp.Views
@@ -15,6 +9,12 @@ namespace UNBlockchainApp.Views
 		public DocumentationPage ()
 		{
 			InitializeComponent ();
-		}
+            var browser = new WebView
+            {
+                Source = "https://github.com/diegobaqt/un_blockchain/blob/master/README.md"
+            };
+
+            Content = browser;
+        }
 	}
 }
